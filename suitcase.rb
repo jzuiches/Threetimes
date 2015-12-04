816e5e3a1d14c81b7c41322bdc66a59f13b4d444
Joe Zuiches <jzuiches@yahoo.com>
2015-12-03 18:47:25 -0800

    prepend vs. include

--- suitcase.rb
+++ suitcase.rb
@@ -27,3 +27,4 @@ ch.load_and_report(sc3)
 first_unloaded = ch.unload
 puts "The first suitcase is: "
 puts first_unloaded.object_id
+# the difference between prepend and include is that r[prepend will cause the object to look in the modelue first before it looks in its own class for the method.
