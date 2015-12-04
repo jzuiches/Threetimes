d114b28387a80084e71c2a09253bbe7466018d98
Joe Zuiches <jzuiches@yahoo.com>
2015-12-03 17:47:58 -0800

    add puts statements to stack.rb

--- stack.rb
+++ stack.rb
@@ -9,3 +9,8 @@ s.add_to_stack("item two")
 s.add_to_stack("item three")
 puts "Objects currently on the stack: "
 puts s.stack
+taken = s.take_from_stack
+puts "Removed this object: "
+puts taken
+puts "Now on stack: "
+puts s.stack
