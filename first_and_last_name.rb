446cc4fb564ec8cc91f038ea89124e9ac25a0acd
Joe Zuiches <jzuiches@yahoo.com>
2015-12-09 06:23:58 -0800

    this is a quick bareword method invocation

---
+++ first_and_last_name.rb
@@ -0,0 +1,17 @@
+class Person
+  attr_accessor :first_name, :middle_name, :last_name
+  def whole_name
+    #uses the first_name accessor without invoking self
+    n = self.first_name + " "
+    n << "#{middle_name} " if self.middle_name
+    n << self.last_name
+  end
+
+end
+
+david = Person.new
+david.first_name = "David"
+david.last_name = "Black"
+puts "David's whole name: #{david.whole_name}"
+david.middle_name = "Alan"
+puts "David's new whole name: #{david.whole_name}"
